03ed42fd445f32fd62cdb4072e059b9233179db4
paranoiase Kang <paranoiase@gmail.com>
2012-07-04 23:01:38 +0900

    should edit and destroy transaction model

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,9 +1,14 @@
 # encoding: UTF-8
 
 class TransactionsController < ApplicationController
+  before_filter :find_transactions
+
   def new
-    @transactions = Transaction.where("")
     @transaction = Transaction.new
+  end
+
+  def edit
+    @transaction = Transaction.find(params[:id])
   end
 
   def create
@@ -11,8 +16,27 @@ class TransactionsController < ApplicationController
     if @transaction.save
       redirect_to :root, notice: "새로운 내역을 만들었습니다."
     else
-      @transactions = Transaction.where("")
       render 'new'
     end
+  end
+
+  def update
+    @transaction = Transaction.find(params[:id])
+    if @transaction.update_attributes(params[:transaction])
+      redirect_to :root, notice: "내역을 수정했습니다."
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @transaction = Transaction.find(params[:id])
+    @transaction.destroy
+    redirect_to :root, notice: "성공적으로 내역을 제거하였습니다."
+  end
+
+  private
+  def find_transactions
+    @transactions = Transaction.where("")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Housekeep::Application.routes.draw do
-  resources :transactions, only: [:new, :create]
+  resources :transactions, only: [:new, :create, :edit, :update, :destroy]
 
   root to: 'welcome#index'
 end
