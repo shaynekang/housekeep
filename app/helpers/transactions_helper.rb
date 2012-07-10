c633f677092bd1c3c9302c8e3dfa27d728a64b01
paranoiase Kang <paranoiase@gmail.com>
2012-07-10 23:50:33 +0900

    extract default_date to helper

---
+++ app/helpers/transactions_helper.rb
@@ -0,0 +1,11 @@
+module TransactionsHelper
+	def default_transacted_at
+		if params[:transacted_at]
+			now = DateTime.now
+			transacted_at = DateTime.parse(params[:transacted_at])
+			transacted_at.change(hour: now.hour, min: now.minute)
+		else
+			@Transactions
+		end
+	end
+end
