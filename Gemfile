614a3dcf492b07f4497fa119d392b0bddcb6e92a
paranoiase Kang <paranoiase@gmail.com>
2012-07-11 21:50:27 +0900

    add google oauth2 authentication

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails-i18n'
 gem 'omniauth'
 gem 'omniauth-twitter'
 gem 'omniauth-facebook'
+gem "omniauth-google-oauth2"
 
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'YtxzwXypu3t8H1D2mk5UjA', 'CQlCxJ4LFOaxmTQEjaiJeWnYwpo1y2PC42CXrLzxoqc'
   provider :facebook, '202284159899227', '42890ff08d4f956227bb1b78f65739f1'
+  provider :google_oauth2, '239519401304.apps.googleusercontent.com', 'y0G-xH2rKEoP0uNynFOmH4Rk'
 end
